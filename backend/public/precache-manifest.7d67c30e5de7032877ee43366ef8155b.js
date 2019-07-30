6a847e407a5a9d3b8e9b0cc3de1251d3f6aa2828
chuca1 <diego.gm99@hotmail.com>
2019-07-30 16:05:56 -0500

    deploy

---
+++ backend/public/precache-manifest.7d67c30e5de7032877ee43366ef8155b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5211802ef5a56978a8d3c2bbfe31878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a4756e7dd9587e73287",
+    "url": "/static/css/2.6d3a7656.chunk.css"
+  },
+  {
+    "revision": "2fe4170056a84143b2be",
+    "url": "/static/css/main.77f587bd.chunk.css"
+  },
+  {
+    "revision": "8a4756e7dd9587e73287",
+    "url": "/static/js/2.dd444838.chunk.js"
+  },
+  {
+    "revision": "2fe4170056a84143b2be",
+    "url": "/static/js/main.a3147eaa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46946042b219e9292d63d4c990cb9dd0",
+    "url": "/static/media/antMotionStyle.46946042.less"
+  },
+  {
+    "revision": "b1d5b62125095d4db77a1c18c3f0d219",
+    "url": "/static/media/fondo.b1d5b621.jpg"
+  }
+]);
